f65d9cf0764ede69bc849686e5594b2f2fb53fe5
linuxbytes <6693857+linuxbytes@users.noreply.github.com>
2019-12-06 21:23:41 -0500

    fix: refactor page

--- js/app.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-  const x = document.getElementsByClassName('menu-link');
-
-  if (x.style.display === 'block') {
-    x.style.display = 'none';
-  } else {
-    x.style.display = 'block';
-  }
-}
